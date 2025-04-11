b7661fe8f16b6175aad4ed7db6b9062ce9735553
sonu07777 <122894139+sonu07777@users.noreply.github.com>
2025-04-11 13:31:46 +0530

    adding backend url

--- Frontend/src/AxiosInstance/authAxio.js
+++ Frontend/src/AxiosInstance/authAxio.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5020";
+// const BASE_URL = "http://localhost:5020";
+const BASE_URL = "https://taskmate-2bqh.onrender.com";
+
 const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
